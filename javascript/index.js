68ee698a6a1de645b9eb99d1e91a15e43c992ca3
alexpfuentes <123893015+alexpfuentes@users.noreply.github.com>
2023-04-06 20:05:06 -0500

    added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,56 @@
 //Los Angeles
 setInterval(function () {
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Paris
-  let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
+  let kyleElement = document.querySelector("#kyle");
+  if (kyleElement) {
+    let kyleDateElement = kyleElement.querySelector(".date");
+    let kyleTimeElement = kyleElement.querySelector(".time");
+    let kyleTime = moment().tz("America/Ojinaga");
 
-  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
-});
+    kyleDateElement.innerHTML = kyleTime.format("MMMM Do YYYY");
+    kyleTimeElement.innerHTML = kyleTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+  //Maracaibo
+  let maracaiboElement = document.querySelector("#maracaibo");
+  let maracaiboDateElement = maracaiboElement.querySelector(".date");
+  let maracaiboTimeElement = maracaiboElement.querySelector(".time");
+  let maracaiboTime = moment().tz("America/Caracas");
+
+  maracaiboDateElement.innerHTML = maracaiboTime.format("MMMM Do YYYY");
+  maracaiboTimeElement.innerHTML = maracaiboTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+    </div>
+  `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
